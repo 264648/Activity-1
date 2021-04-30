4f4880d3fab84bdfd94ad36c9eb70b81f129dd35
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:03:45 +0530

    Update ledstat.c

--- src/ledstat.c
+++ src/ledstat.c
@@ -1,1 +1,22 @@
+/**
+ * @file ledstat.c
+ * @author Sai Rishitha
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<avr/io.h>
+#include "activity1.h"
 
+/**
+ * @brief Led change state
+ * 
+ * @param st 
+ */
+void led(uint8_t st)
+{
+    LED_PORT=(st<<LED_PIN);
+}
