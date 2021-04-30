2505223a5362e1da471f3c3225664a21c3abae2c
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:05:19 +0530

    Add files via upload

---
+++ src/ReadADC.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void InitADC()
+{
+    ADMUX=(1<<REFS0); // For Aref=AVcc;
+    ADCSRA=(1<<ADEN) | (7<<ADPS0);
+}
+uint16_t ReadADC(uint8_t ch)
+{
+    //SELECT ADC CHANNEL CH MUST BE 0-7
+    ADMUX&=0Xf8;
+    ch=ch&0b00000111;
+    ADMUX|=ch;
+    // Start Single conversion
+    ADCSRA|=(1<<ADSC);
+    // Wait for conversion to complete
+    while(!(ADCSRA & (1<<ADIF)));
+    //Clear ADIF by writing one to it
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+}
