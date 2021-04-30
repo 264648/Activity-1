57b45acd48c618535c50991a18a1e0eea3e23953
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:49:00 +0530

    Update and rename ledstat.c to act1.c

--- src/act1.c
+++ src/act1.c
@@ -1,5 +1,5 @@
 /**
- * @file ledstat.c
+ * @file act1.c
  * @author Sai Rishitha
  * @brief 
  * @version 0.1
@@ -9,7 +9,7 @@
  * 
  */
 #include<avr/io.h>
-#include "activity1.h"
+#include "act1.h"
 
 /**
  * @brief Led change state
@@ -19,4 +19,11 @@
 void led(uint8_t st)
 {
     LED_PORT=(st<<LED_PIN);
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    PORTD|=(1<<PD1);
 }
