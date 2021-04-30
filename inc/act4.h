03570e97e72c64e5dde4af6b012f9974c63857a0
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:43:05 +0530

    Create act4.h

---
+++ inc/act4.h
@@ -0,0 +1,24 @@
+/**
+ * @file act4.h
+ * @author Sai Rishitha
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __ACT4_H__
+#define __ACT4_H__
+#include<avr/io.h>
+/**
+ * @brief Inittialize UART serial communication ports
+ * 
+ * @param ubrr 
+ */
+void InitUART(uint16_t ubrr);
+#endif
+/**
+ * @brief End
+ * 
+ */
