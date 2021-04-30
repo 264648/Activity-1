c90f7a24dd331bd314d6c123c0694388c7c3a2b6
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:38:02 +0530

    Create act3.h

---
+++ inc/act3.h
@@ -0,0 +1,26 @@
+/**
+ * @file act3.h
+ * @author Sai Rishitha
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __act3_H__
+#define __act3_H__
+#include<avr/io.h>
+/**
+ * @brief Reading Analog values
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+char OutP(uint16_t );
+void InitP();
+#endif
+/**
+ * @brief end
+ * 
+ */
