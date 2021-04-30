1c2d15e3a599c267b1d4c4bcc14b875d3bea1d6f
264648 <81163246+264648@users.noreply.github.com>
2021-04-30 11:57:22 +0530

    Update and rename UART.c to act4.c

--- src/act4.c
+++ src/act4.c
@@ -1,5 +1,5 @@
 /**
- * @file UART.c
+ * @file act4.c
  * @author Sai Rishitha
  * @brief 
  * @version 0.1
@@ -9,7 +9,7 @@
  * 
  */
 #include<avr/io.h>
-#include "UART.h"
+#include "act4.h"
 /**
  * @brief Initialization
  * 
@@ -22,4 +22,17 @@ void USARTInit(uint16_t ubrr_value)
     UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
     UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
     UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+}
+/**
+ * @brief write data on UART ports
+ * 
+ * @param d
+ */
+void UARTwrite(char d)
+{
+    while(!(UCSR0A & (1<<UDRE0)))
+    {
+
+    }
+    UDR0 = d;
 }
